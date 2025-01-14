b6575c9744b0a767f60350721988cf6ed59364d8
Casey <cmillstein1@me.com>
2025-01-13 23:56:55 -0500

    Create project

---
+++ fetch-takehome/fetch-takehome/fetch_takehomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_takehomeApp.swift
+//  fetch-takehome
+//
+//  Created by Casey Millstein New on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct fetch_takehomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fetch-takehome/fetch-takehomeTests/fetch_takehomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_takehomeTests.swift
+//  fetch-takehomeTests
+//
+//  Created by Casey Millstein New on 1/13/25.
+//
+
+import Testing
+@testable import fetch_takehome
+
+struct fetch_takehomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
